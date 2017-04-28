52970e18c0861206241d05da9482098c02427b07
dell-PC\dell <radicho123@gmail.com>
2017-04-28 20:59:50 +0300

    changes in index page

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import RSGLabel from "../../lib/RSGLabel";
+import RSGBox from "../../lib/RSGBox";
 
 class IndexPage extends Component {
   render(){
     return(
       <div>
-        <h1>Hello to rsg-components!</h1>
+        <h1>
+          Hello to rsg-components! Cool front-end framework built for <a href="https://facebook.github.io/react/">React.js</a><br/>
+        </h1>
+        Get started with React - https://facebook.github.io/react/docs, view on GitHub: https://github.com/facebook/react <br/>
+        <h2>Example:</h2>
+        <div id="example1">
+          <b>rsg-components <i>v2.0.13</i></b> <RSGLabel labelType={`s`}>stable</RSGLabel> <RSGBox>In this version was added RSGCheckbox component</RSGBox>
+        </div>
       </div>
     );
   }
