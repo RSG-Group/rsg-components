5f095c246f74d998c258fa9cb47c0ab026423a68
dell-PC\dell <radicho123@gmail.com>
2017-04-29 20:06:20 +0300

    added example in index page

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -2,8 +2,26 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import RSGLabel from "../../lib/RSGLabel";
 import RSGBox from "../../lib/RSGBox";
+import RSGCheckbox from "../../lib/RSGCheckbox";
 
 class IndexPage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      checked: false,
+      status: "Status missing"
+    }
+  }
+
+  __handleChange(){
+    this.setState({ checked: !this.state.checked });
+    if(!this.state.checked){
+      this.setState({ status: "If you have problems, please report an issue on Github" });
+    }else{
+      this.setState({ status: "No problems" });
+    }
+  }
+
   render(){
     return(
       <div>
@@ -13,8 +31,11 @@ class IndexPage extends Component {
         Get started with React - https://facebook.github.io/react/docs, view on GitHub: https://github.com/facebook/react <br/>
         <h2>Example:</h2>
         <div id="example1">
-          <b>rsg-components <i>v2.0.13</i></b> <RSGLabel labelType={`s`}>stable</RSGLabel>
+          <b>rsg-components <i>v2.0.13</i></b> <RSGLabel labelType={`s`}>stable</RSGLabel><br/>
+          Any problems? <RSGCheckbox onChange={this.__handleChange.bind(this)}/> <br/>
+          <b>Status:</b> { this.state.status }
         </div>
+        Here we use <code>RSGLabel</code> and <code>RSGCheckbox</code> from <code>rsg-components</code> 
       </div>
     );
   }
