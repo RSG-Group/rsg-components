bcb3bcc079099c53648bce4b11cc52e113ec975f
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2017-07-14 21:59:18 +0530

    Fix Code Climate, update shrinkwrap and refactor RSGProgressBar. FInal refactors and tweaks and tests on RSGCheckbox left after which 3.0 should have it's beta release.

--- src/RSGProgressBar.js
+++ src/RSGProgressBar.js
@@ -13,26 +13,46 @@ type Props = {
   progressCount: number,
 };
 
+function ReactInvalidPropException(info: {
+  prop: string,
+  type: string,
+  expectedValue?: string,
+  actualValue?: string,
+}) {
+  this.name = "ReactInvalidPropException";
+  if (info.type === "invalid-type") {
+    this.message = `RSGProgressBar has incorrectly recieved a ${info.actualValue} for the prop "${info.prop}" when it expected a ${info.expectedValue}.`;
+  } else if (info.type === "unassigned-prop") {
+    this.message = `RSGProgressBar has not recieved the required prop "${info.prop}". Click "OK" to rectify.`;
+  }
+}
+
 export default function RSGProgressBar(props: Props) {
   // Ugly code.
-  if (!props.anim && !props.progressCount && typeof props.progressCount !== "number") {
-    // Weird code.
-    return <div><b style={{ color: "red" }}>You MUST set progressCount prop as number!!!</b></div>;
+  if (!props.progressCount || typeof props.progressCount !== "number") {
+    throw new ReactInvalidPropException({
+      prop: "progressCount",
+      type: "invalid-type",
+      expectedValue: "number",
+      actualValue: typeof props.progressCount,
+    });
   }
   if (!props.id) {
-    // Weird code.
-    return <div><b style={{ color: "red" }}>You MUST set id of the RSGProgressBar component!</b></div>;
+    throw new ReactInvalidPropException({
+      prop: "id",
+      type: "unassigned-prop",
+    });
   }
 
   // Ugly code.
-  let style = `.RSGProgressBar{  background: rgb(210, 215, 215);  box-shadow: inset 0 1.5px 3px rgb(150, 150, 150);  -webkit-box-shadow: inset 0 1.5px 3px rgb(150, 150, 150);  border-radius: 3px;  height: 20px;} #${props.id}SPAN{  background: rgb(51, 122, 183);  transition: width 1s;  width: 1%;  height: 20px;  border-radius: 5px;  text-align: center;  color: white; }`;
+  let style = `.RSGProgressBar{  background: rgb(210, 215, 215);  box-shadow: inset 0 1.5px 3px rgb(150, 150, 150);  -webkit-box-shadow: inset 0 1.5px 3px rgb(150, 150, 150);  border-radius: 3px;  height: 20px;} #${props.id}SPAN{  background: rgb(51, 122, 183);  transition: width 1s;  width: 1%;  height: 20px;  border-radius: 5px;  text-align: center;  color: white; } .PP { overflow: hidden; }`;
 
   if (props.checkered === true) {
     // Ugly code.
-    style = ".RSGProgressBar { background: rgb(210, 215, 215); box-shadow: inset 0 1.5px 3px rgb(150, 150, 150); -webkit-box-shadow: inset 0 1.5px 3px rgb(150, 150, 150); border-radius: 3px; height: 20px; } .PP { background: url(https://raw.githubusercontent.com/RSG-Group/rsg-components/master/images/Progress.png); transition: width 1s; height: 20px; border-radius: 5px;text-align: center; color: white; }";
+    style = ".RSGProgressBar { background: rgb(210, 215, 215); box-shadow: inset 0 1.5px 3px rgb(150, 150, 150); -webkit-box-shadow: inset 0 1.5px 3px rgb(150, 150, 150); border-radius: 3px; height: 20px; } .PP { overflow: hidden; background: url(https://raw.githubusercontent.com/RSG-Group/rsg-components/master/images/Progress.png); transition: width 1s; height: 20px; border-radius: 5px;text-align: center; color: white; }";
   } else if (props.checkered === "animated") {
     // Ugly code.
-    style = ".RSGProgressBar { background: rgb(210, 220, 220); box-shadow: inset 0 1.5px 3px rgb(150, 150, 150); -webkit-box-shadow: inset 0 1.5px 3px rgb(150, 150, 150); border-radius: 3px; height: 20px; } @keyframes animatedBackground { from { background-position: 0 0; } to { background-position: 100% 0; } } .PP { background-image: url(https://raw.githubusercontent.com/RSG-Group/rsg-components/master/images/Progress.png); background-position: 0px 0px; background-repeat: repeat-x; animation: animatedBackground 75s linear infinite; transition: width 1s; height: 20px; border-radius: 5px; text-align: center; color: white; }";
+    style = ".RSGProgressBar { background: rgb(210, 220, 220); box-shadow: inset 0 1.5px 3px rgb(150, 150, 150); -webkit-box-shadow: inset 0 1.5px 3px rgb(150, 150, 150); border-radius: 3px; height: 20px; } @keyframes animatedBackground { from { background-position: 0 0; } to { background-position: 100% 0; } } .PP { background-image: url(https://raw.githubusercontent.com/RSG-Group/rsg-components/master/images/Progress.png); background-position: 0px 0px; background-repeat: repeat-x; animation: animatedBackground 75s linear infinite; overflow: hidden; transition: width 1s; height: 20px; border-radius: 5px; text-align: center; color: white; }";
   }
 
   // What?
@@ -50,10 +70,6 @@ export default function RSGProgressBar(props: Props) {
   if (props.anim === true && props.children) {
     console.error("Now you are in \"anim\"-mode. Move your \"children\" to arguments in anim() function !!!");
   }
-
-  // *yawn* What?
-  style += " .PP{ overflow: hidden; }";
-
 
   return (
     <div>
