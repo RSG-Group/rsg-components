e6a6be3e7ea968ba228f882728f7587a545d3ba9
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2017-07-15 21:20:56 +0530

    Document ProgressBar with some minor refactors.

--- README.md
+++ README.md
@@ -1,27 +1,41 @@
 # RSG-Components
+
 ## Badges
+
 [![RSG-Group Slack Team](https://rsg-slack.herokuapp.com/badge.svg)](https://rsg-slack.herokuapp.com)
-#### Dependencies
+
+### Dependencies
+
 [![dependencies Status](https://david-dm.org/RSG-Group/rsg-components/status.svg?style=flat-square)](https://david-dm.org/RSG-Group/rsg-components) [![devDependencies Status](https://david-dm.org/RSG-Group/rsg-components/dev-status.svg?style=flat-square)](https://david-dm.org/RSG-Group/rsg-components?type=dev) [![peerDependencies Status](https://david-dm.org/RSG-Group/rsg-components/peer-status.svg?style=flat-square)](https://david-dm.org/RSG-Group/rsg-components?type=peer)
-#### Coverage and Linting
+
+### Coverage and Linting
+
 [![Code Climate](https://img.shields.io/codeclimate/github/RSG-Group/rsg-components.svg?style=flat-square)](https://codeclimate.com/github/RSG-Group/rsg-components) [![Code Climate](https://img.shields.io/codeclimate/issues/github/RSG-Group/rsg-components.svg?style=flat-square)](https://codeclimate.com/github/RSG-Group/rsg-components)
-#### Miscellaneous
+
+### Miscellaneous
+
 [![npm](https://img.shields.io/npm/dm/rsg-components.svg?style=flat-square)](http://npmjs.com/package/rsg-components) [![npm](https://img.shields.io/npm/v/rsg-components.svg?style=flat-square)](http://npmjs.com/package/rsg-components) [![npm (tag)](https://img.shields.io/npm/v/rsg-components/next.svg?style=flat-square)](http://npmjs.com/package/rsg-components) [![npm](https://img.shields.io/npm/l/rsg-components.svg?style=flat-square)](http://npmjs.com/package/rsg-components) [![license](https://img.shields.io/badge/license-Apache--2.0-blue.svg)]()
 
 Install via npm.
-```
+
+```zsh
 npm install rsg-components
 ```
+
 Install via yarn, a futuristic package manager.
-```
+
+```zsh
 yarn add rsg-components
 ```
+
 or clone the repository!
-```
+
+```zsh
 git clone https://github.com/RSG-Group/rsg-components
 ```
 
-# Compatibility
+## Compatibility
+
 - Firefox > 31
 - Chrome > 37
 - IE < 11 unsupported.
@@ -29,7 +43,8 @@ git clone https://github.com/RSG-Group/rsg-components
 - Microsoft Edge supported.
 - Opera versions which use Blink are supported.
 
-# Example
+## Example
+
 ```jsx
 import { render } from "react-dom";
 import React from "react";
@@ -37,11 +52,11 @@ import { RSGCheckbox } from "rsg-components";
 
 render(<RSGCheckbox props>children</RSGCheckbox>, document.getElementById("app"));
 ```
-#### If using RSGProgressBar, include `node_modules/rsg-components/lib/index.css` in your HTML. If RSGCheckbox does not work, then use this CSS file for that component as well.
+
+### If using RSGProgressBar, include `node_modules/rsg-components/lib/index.css` in your HTML. If RSGCheckbox does not work, then use this CSS file for that component as well
 
 View all components, props and styles on the GitHub wiki page: [https://github.com/RSG-Group/rsg-components/wiki/](https://github.com/RSG-Group/rsg-components/wiki/)
 
-
-# [Tutorial on YouTube by @Radi123](https://www.youtube.com/playlist?list=PLBKNj-_iswgrl1XNhpfgGKwfrP_hEoZ2x)
+## [Tutorial on YouTube by @Radi123](https://www.youtube.com/playlist?list=PLBKNj-_iswgrl1XNhpfgGKwfrP_hEoZ2x)
 
 ### [Link to channel here.](https://www.youtube.com/c/RadiCho)
--- src/RSGProgressBar.js
+++ src/RSGProgressBar.js
@@ -1,34 +1,41 @@
-// @flow weak
+// @flow
 /* eslint-disable no-console */
+// Import React.
 import React from "react";
 
-let txt = "Initializing";
-
+// Ink out the type of our props.
 type Props = {
   anim?: boolean,
   id: string,
   checkered?: boolean | string,
-  children: Array<React.createElement>,
-  text?: string,
+  children: string,
+  text: string,
   progressCount: number,
 };
 
+// A function to define the error thrown on invalid props.
 function ReactInvalidPropException(info: {
   prop: string,
   type: string,
   expectedValue?: string,
   actualValue?: string,
 }) {
+  // Actual code starts here.
   this.name = "ReactInvalidPropException";
-  if (info.type === "invalid-type") {
-    this.message = `RSGProgressBar has incorrectly recieved a ${info.actualValue} for the prop "${info.prop}" when it expected a ${info.expectedValue}.`;
+  // Extract values from info to make code line length smaller.
+  const { type, prop, expectedValue, actualValue } = info;
+  // If..
+  if (type === "invalid-type" && expectedValue !== undefined && actualValue !== undefined) {
+    this.message = `RSGProgressBar has incorrectly recieved a ${actualValue} for the prop "${prop}" when it expected a ${expectedValue}.`;
+  // Else..
   } else if (info.type === "unassigned-prop") {
-    this.message = `RSGProgressBar has not recieved the required prop "${info.prop}". Click "OK" to rectify.`;
+    this.message = `RSGProgressBar has not recieved the required prop "${prop}". Click "OK" to rectify.`;
   }
 }
 
+// Our component.
 export default function RSGProgressBar(props: Props) {
-  // Ugly code.
+  // If progressCount is not a number then.
   if (!props.progressCount || typeof props.progressCount !== "number") {
     throw new ReactInvalidPropException({
       prop: "progressCount",
@@ -37,6 +44,7 @@ export default function RSGProgressBar(props: Props) {
       actualValue: typeof props.progressCount,
     });
   }
+  // If the prop `id` is undefined..
   if (!props.id) {
     throw new ReactInvalidPropException({
       prop: "id",
@@ -55,34 +63,33 @@ export default function RSGProgressBar(props: Props) {
     style = ".RSGProgressBar { background: rgb(210, 220, 220); box-shadow: inset 0 1.5px 3px rgb(150, 150, 150); -webkit-box-shadow: inset 0 1.5px 3px rgb(150, 150, 150); border-radius: 3px; height: 20px; } @keyframes animatedBackground { from { background-position: 0 0; } to { background-position: 100% 0; } } .PP { background-image: url(https://raw.githubusercontent.com/RSG-Group/rsg-components/master/images/Progress.png); background-position: 0px 0px; background-repeat: repeat-x; animation: animatedBackground 75s linear infinite; overflow: hidden; transition: width 1s; height: 20px; border-radius: 5px; text-align: center; color: white; }";
   }
 
-  // What?
+  // What? Replace with check for props.children and props.text should not co-exist.
   if (props.children && typeof props.children !== "string") {
     console.error("Please set only text for children");
   }
 
-  // Weird code.
-  // flow-disable-next-line
-  txt = props.children.length > 0 ? `${props.progressCount}% ${props.text}` : `${props.progressCount}% ${props.text}`;
-  // Weird code.
-  const PPwidth = `${props.progressCount}%`;
+  // Content to be included on the progress bar.
+  const text = props.children.length > 0 ? `${props.progressCount}% ${props.children}` : `${props.progressCount}% ${props.text}`;
 
   // Weird block of code.
   if (props.anim === true && props.children) {
     console.error("Now you are in \"anim\"-mode. Move your \"children\" to arguments in anim() function !!!");
   }
 
+  // Gulp. We need to see what to do about this :3
   return (
     <div>
       <style key={1}>{style}</style>
       <div className="RSGProgressBar" key={5} {...props}>
-        <div className="PP" id={`${props.id}SPAN`} style={{ width: PPwidth }} key={3}>
-          {props.children}{txt}
+        <div className="PP" id={`${props.id}SPAN`} style={{ width: `${props.progressCount}%` }} key={3}>
+          {text}
         </div>
       </div>
     </div>
   );
 }
 
+// Our default props.
 RSGProgressBar.defaultProps = {
   anim: false,
   checkered: false,
